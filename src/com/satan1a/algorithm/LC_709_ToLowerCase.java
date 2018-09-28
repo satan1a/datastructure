f8a19676a1f3c071be1ad45cf9b4141b8af7be5d
toky <979564561@qq.com>
2018-09-28 11:10:05 +0800

    第 1 天

--- src/com/satan1a/algorithm/LC_709_ToLowerCase.java
+++ src/com/satan1a/algorithm/LC_709_ToLowerCase.java
@@ -8,11 +8,30 @@ import java.util.ArrayList;
  * @date 2018.9.28
  */
 public class LC_709_ToLowerCase {
+
+    public static void main(String[] args) {
+        LC_709_ToLowerCase question = new LC_709_ToLowerCase();
+        System.out.printf(question.toLowerCase("abcDEF123"));
+
+    }
+
     public String toLowerCase(String str) {
-        ArrayList<String> stringArrayList = new ArrayList<>();
 
-
-
+        // 使用 toCharArray()方法 将字符串转化为一个 Character字符数组
+        char[] charsList = str.toCharArray();
+        // 使用 StringBuffer 储存最后转化后输出的字符串
+        StringBuffer stringBuffer = new StringBuffer();
+        // 循环遍历数组
+        for (int i = 0; i < charsList.length; i++) {
+            // 根据ASCII, 将大写字母字符 转化为小写字母字符 +32
+            if (charsList[i] < 97 && charsList[i] >= 65) {
+                charsList[i] += 32;
+            }
+            // append() 方法 将转化后的字符添加到 StringBuffer 对象最后面
+            stringBuffer.append(charsList[i]);
+        }
+        // toString() 方法转化为字符串输出
+        return stringBuffer.toString();
     }
 
 }
